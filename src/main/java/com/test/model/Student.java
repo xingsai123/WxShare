8db60560fa57367a646e4cd5bdbfab41c946eb10
15173 <15173126889@163.com>
2018-06-02 23:58:54 +0800

    微信js接口认证注入信息...

---
+++ src/main/java/com/test/model/Student.java
@@ -0,0 +1,54 @@
+package com.test.model;
+
+public class Student {
+    private Integer sid;
+
+    private String sname;
+
+    private String ssex;
+
+    private Integer sage;
+
+    public Student(Integer sid, String sname, String ssex, Integer sage) {
+        this.sid = sid;
+        this.sname = sname;
+        this.ssex = ssex;
+        this.sage = sage;
+    }
+
+    public Student() {
+        super();
+    }
+
+    public Integer getSid() {
+        return sid;
+    }
+
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname == null ? null : sname.trim();
+    }
+
+    public String getSsex() {
+        return ssex;
+    }
+
+    public void setSsex(String ssex) {
+        this.ssex = ssex == null ? null : ssex.trim();
+    }
+
+    public Integer getSage() {
+        return sage;
+    }
+
+    public void setSage(Integer sage) {
+        this.sage = sage;
+    }
+}
---
+++ src/main/java/com/test/service/IStudentService.java
@@ -0,0 +1,9 @@
+package com.test.service;
+
+import com.test.model.Student;
+
+public interface IStudentService {
+
+  public Student loginVertifiled(int sid);
+
+}
