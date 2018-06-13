2081de37fa08cb6e9fd0a2e16573ef604d4035e0
15173 <15173126889@163.com>
2018-06-13 18:38:53 +0800

    微信js接口认证注入信息...

--- src/main/java/com/test/controller/WxController.java
+++ src/main/java/com/test/controller/WxController.java
@@ -16,30 +16,6 @@ import java.util.Map;
 public class WxController {
 
 
-    @RequestMapping(value = "/getWxInfo")
-    @ResponseBody
-   public ModelAndView getWxInfo(HttpServletRequest request, HttpServletResponse response){
-        System.out.println("微信注入信息！");
-        String url1= request.getParameter("url1");
-        ModelAndView ma=new ModelAndView("success");
-
-        url1="http://www.maomi.xn--fiqs8s/getDetails/1524409397585";
-
-      if (!" ".equals(url1) && url1!=null){
-         Map map=WxconfigUtil.getWxInfo(url1);
-          System.out.println(map);
-          ma.addObject("res", map);
-
-      }
-       else {
-          System.out.println("url为空！");
-          ma.addObject("error","url空");
-      }
-      return ma;
-    }
-
-
-
     @RequestMapping(value = "/getWxInfo1")
     @ResponseBody
     public void getWxInfo1(HttpServletRequest request, HttpServletResponse response){
@@ -63,7 +39,7 @@ public class WxController {
                    printWriter.flush();
                    printWriter.close();
                }
-           };
+           }
         }
         else {
             System.out.println("url为空！");
